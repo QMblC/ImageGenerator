06910e29dac5c9669f9e8790770830864905a033
kurto <kurtovnevg@gmail.com>
2025-04-24 21:02:45 +0500

    Trying get data

--- ImageGenerator/Program.cs
+++ ImageGenerator/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем сервисы для поддержки контроллеров
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Обработчик для корневого пути "/"
+app.MapGet("/", () => "Сервер работает!");
+app.MapGet("/api/create-sample/", () => HandleRequest());
+
+
+// Маппинг контроллеров
+app.MapControllers();   
+
+// Запуск приложения на порту 5000
+app.Run("http://localhost:5000");
+
+static string HandleRequest()
+{
+    return "Hello world!";
+}
+
+public class ImageJson
+{
+    public double NameXPos;
+    public double NameYPos;
+    public double ScaleFactor;
+    public int FontSize;
+
+}
+
